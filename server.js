f59dd39416366fa020da344b9a86fafa2b129af6
Ashish Chawla <ashish@reportcard.ae>
2020-05-09 22:36:42 +0530

    Setting up Basic Routes

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started running on port: ${PORT}`));
 
+//Define Routes
 app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
