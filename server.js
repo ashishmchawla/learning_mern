744fed324d20c5b66326d8d67e160b21b7371e52
Ashish Chawla <ashish@reportcard.ae>
2020-05-09 20:21:38 +0530

    Connecting to Mongo DB using Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+//Connection to Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
